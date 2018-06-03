1a75ad8c949441d019c7c0c6454e6386c8e5580f
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-03 22:02:24 +0200

    Video in the background

--- RaspberryPiMonitor/ViewController.swift
+++ RaspberryPiMonitor/ViewController.swift
@@ -11,6 +11,8 @@ import AVKit
 import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet var outlet: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,20 +24,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playVideo(_ sender: UIButton) {
-        guard let url = URL(string: "https://devimages-cdn.apple.com/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
+        guard let url = URL(string: "http://www.html5videoplayer.net/videos/toystory.mp4") else {
             return
         }
+        outlet.alpha = 0.75
+        outlet.layer.zPosition = 0
+        
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
         let player = AVPlayer(url: url)
-        
+        player.actionAtItemEnd = .none
+        player.isMuted = false
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        playerLayer.zPosition = -1
+        playerLayer.frame = view.frame
+    
+        view.layer.addSublayer(playerLayer)
+
         // Create a new AVPlayerViewController and pass it a reference to the player.
-        let controller = AVPlayerViewController()
-        controller.player = player
+//        let controller = AVPlayerViewController()
+//        controller.player = player
         
         // Modally present the player and call the player's play() method when complete.
-        present(controller, animated: true) {
+//        present(controller, animated: true) {
             player.play()
-        }
+//        }
     }
     
 }
