0f6371bbbda9899db21622c1c5eec34ced428948
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-03 21:24:12 +0200

    Init

--- RaspberryPiMonitor/AppDelegate.swift
+++ RaspberryPiMonitor/AppDelegate.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
         return true
     }
 
--- RaspberryPiMonitor/Jobs/Player.swift
+++ RaspberryPiMonitor/Jobs/Player.swift
@@ -1,42 +1,31 @@
 //
-//  ViewController.swift
+//  Player.swift
 //  RaspberryPiMonitor
 //
 //  Created by Szymon Nowicki on /03/0618.
 //  Copyright © 2018 Szymon Nowicki. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import AVKit
 import AVFoundation
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func playVideo(_ sender: UIButton) {
-        guard let url = URL(string: "https://devimages-cdn.apple.com/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
+class Player {
+    static func play(href: String) {
+        guard let url = URL(string: href) else {
             return
         }
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
         let player = AVPlayer(url: url)
-        
+    
         // Create a new AVPlayerViewController and pass it a reference to the player.
         let controller = AVPlayerViewController()
         controller.player = player
-        
+    
         // Modally present the player and call the player's play() method when complete.
         present(controller, animated: true) {
             player.play()
         }
     }
-    
 }
-
