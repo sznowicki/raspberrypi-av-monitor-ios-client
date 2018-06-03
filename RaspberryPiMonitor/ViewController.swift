07cd2bb2f7988236f066017e09f153228285646d
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-03 20:57:50 +0200

    Initial Commit

---
+++ RaspberryPiMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RaspberryPiMonitor
+//
+//  Created by Szymon Nowicki on /03/0618.
+//  Copyright © 2018 Szymon Nowicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
