cb73dc5f936f47be9e59246dfe269af1eb1c8399
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-09 21:21:06 +0200

    POC - mjpeg stream from rpi server

--- RaspberryPiMonitor/ViewController.swift
+++ RaspberryPiMonitor/ViewController.swift
@@ -7,20 +7,28 @@
 //
 
 import UIKit
-import AVKit
-import AVFoundation
+import MJPEGStreamLib
+
 class ViewController: UIViewController {
 
     @IBOutlet var outlet: UIView!
 
+    @IBOutlet weak var VideoStreamImage: UIImageView!
+    
     @IBOutlet weak var PlayButton: UIButton!
+    
     var isReady = false
+    var stream: MJPEGStreamLib!
+    
     override func viewWillAppear(_ animated: Bool) {
+        // Z-index
+        VideoStreamImage.isHidden = true
         if (!isReady) {
             PlayButton.isHidden = true
         }
         // TODO make it dynamic
         PlayButton.isHidden = false
+        playStream()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,27 +39,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func playVideo(_ sender: UIButton) {
-        guard let url = URL(string: "http://www.html5videoplayer.net/videos/toystory.mp4") else {
+    @IBAction func startTouchUp(_ sender: Any) {
+        playStream()
+    }
+    
+    func playStream() {
+        PlayButton.isHidden = true
+        VideoStreamImage.isHidden = false
+        guard let url = URL(string: "http://rpis.local:8080/?action=stream") else {
             return
         }
-        outlet.alpha = 0.75
-        outlet.layer.zPosition = 0
-        
-        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        let player = AVPlayer(url: url)
-        player.actionAtItemEnd = .none
-        player.isMuted = false
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        playerLayer.zPosition = -1
-        playerLayer.frame = view.frame
-    
-        view.layer.addSublayer(playerLayer)
-
-        player.play()
-        PlayButton.isHidden = true
+        // Set the ImageView to the stream object
+        stream = MJPEGStreamLib(imageView: VideoStreamImage)
+        stream.contentURL = url
+        stream.play()
     }
     
 }
